c429c998f99f35348c73fe0c75846c929b5806ad
Jinfeng Ni <jni@apache.org>
2022-06-07 12:02:12 -0700

    refactor duplicate codes in sql <--> substrait conversion

--- isthmus/src/main/java/io/substrait/isthmus/SubstraitToSql.java
+++ isthmus/src/main/java/io/substrait/isthmus/SubstraitToSql.java
@@ -2,68 +2,35 @@ package io.substrait.isthmus;
 
 import io.substrait.relation.Rel;
 import io.substrait.type.NamedStruct;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
 import org.apache.calcite.config.NullCollation;
 import org.apache.calcite.jdbc.CalciteSchema;
-import org.apache.calcite.jdbc.LookupCalciteSchema;
-import org.apache.calcite.prepare.CalciteCatalogReader;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.rel2sql.RelToSqlConverter;
-import org.apache.calcite.schema.Table;
 import org.apache.calcite.sql.SqlDialect;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.SqlWriterConfig;
 import org.apache.calcite.sql.parser.SqlParseException;
-import org.apache.calcite.sql.validate.SqlValidator;
 
 public class SubstraitToSql extends SqlConverterBase {
-  protected final CalciteCatalogReader catalogReader;
-  protected final SqlValidator validator;
 
   public SubstraitToSql() {
     CalciteSchema rootSchema = CalciteSchema.createRootSchema(false);
-    this.catalogReader = new CalciteCatalogReader(rootSchema, Arrays.asList(), factory, config);
-    this.validator =
-        SqlToSubstrait.Validator.create(factory, catalogReader, SqlValidator.Config.DEFAULT);
   }
 
   public RelNode substraitRelToCalciteRel(Rel relRoot, List<String> tables)
       throws SqlParseException {
-    if (tables != null) {
-      for (String tableDef : tables) {
-        List<SqlToSubstrait.DefinedTable> tList = parseCreateTable(factory, validator, tableDef);
-        for (SqlToSubstrait.DefinedTable t : tList) {
-          catalogReader.getRootSchema().add(t.getName(), t);
-        }
-      }
-    }
-
-    return SubstraitRelNodeConverter.convert(relRoot, relOptCluster, catalogReader, parserConfig);
+    var pair = registerCreateTables(tables);
+    return SubstraitRelNodeConverter.convert(relRoot, relOptCluster, pair.right, parserConfig);
   }
 
   public RelNode substraitRelToCalciteRel(
       Rel relRoot, Function<List<String>, NamedStruct> tableLookup) throws SqlParseException {
-    Function<List<String>, Table> lookup =
-        id -> {
-          NamedStruct table = tableLookup.apply(id);
-          if (table == null) {
-            return null;
-          }
-          return new DefinedTable(
-              id.get(id.size() - 1),
-              factory,
-              TypeConverter.convert(factory, table.struct(), table.names()));
-        };
+    var pair = registerCreateTables(tableLookup);
 
-    CalciteSchema rootSchema = LookupCalciteSchema.createRootSchema(lookup);
-    CalciteCatalogReader catalogReader =
-        new CalciteCatalogReader(rootSchema, List.of(), factory, config);
-    SqlValidator validator = Validator.create(factory, catalogReader, SqlValidator.Config.DEFAULT);
-
-    return SubstraitRelNodeConverter.convert(relRoot, relOptCluster, catalogReader, parserConfig);
+    return SubstraitRelNodeConverter.convert(relRoot, relOptCluster, pair.right, parserConfig);
   }
 
   private static final SqlDialect DEFAULT_DIALECT =
