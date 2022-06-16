12ff240ac892795bc3d03d01425bc4c1944ec65a
Ashvin Agrawal <ashvin@apache.org>
2022-06-16 13:35:36 -0700

    Ignore SqlKeyConstraints while parsing table definitions
    SQL create-table statements often contain key constraints, for e.g.
    table primary key declarations. These constraint declaration are valid
    per SQL standard. Currently the SQL to Substrait conversion fails on
    encountering constraints. This change ignores such declarations when
    present.
    Closes #58

---
+++ isthmus/src/test/java/io/substrait/isthmus/TpcdsQueryNoValidation.java
@@ -0,0 +1,24 @@
+package io.substrait.isthmus;
+
+import com.google.protobuf.util.JsonFormat;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+
+public class TpcdsQueryNoValidation extends PlanTestBase {
+
+  /**
+   * This test only validates that generating substrait plans for TPC-DS queries does not fail. As
+   * of now this test does not validate correctness of the generated plan
+   */
+  @ParameterizedTest
+  @ValueSource(ints = {7})
+  public void tpcds(int query) throws Exception {
+    SqlToSubstrait s = new SqlToSubstrait();
+    String[] values = asString("tpcds/schema.sql").split(";");
+    var creates = Arrays.stream(values).filter(t -> !t.trim().isBlank()).toList();
+    var plan = s.execute(asString(String.format("tpcds/queries/%02d.sql", query)), creates);
+    System.out.println(JsonFormat.printer().print(plan));
+  }
+}
