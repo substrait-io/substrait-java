8cef99917a6098afd5bc53911a5332ad27022afe
david dali susanibar arce <davi.sarces@gmail.com>
2022-10-27 06:22:34 -0500

    build: adding publishing by modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,38 @@ plugins {
   id("com.diffplug.spotless") version "6.11.0"
 }
 
+// publishing { publications { create<MavenPublication>("maven") { from(components["java"]) } } }
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GITHUB"
+            val releasesRepoUrl = System.getenv("GITHUB_URL").takeUnless { it.isNullOrEmpty() } ?: extra["GITHUB_URL"].toString()
+            url = uri(releasesRepoUrl)
+            credentials {
+                username = System.getenv("GITHUB_ACTOR").takeUnless { it.isNullOrEmpty() } ?: extra["GITHUB_ACTOR"].toString()
+                password = System.getenv("GITHUB_TOKEN").takeUnless { it.isNullOrEmpty() } ?: extra["GITHUB_TOKEN"].toString()
+            }
+        }
+        maven {
+            name = "NEXUS"
+            val nexusUrl = System.getenv("NEXUS_URL").takeUnless { it.isNullOrEmpty() } ?: extra["NEXUS_URL"].toString()
+            val releasesRepoUrl = nexusUrl + "/maven-releases"
+            val snapshotsRepoUrl = nexusUrl + "/maven-snapshots"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            isAllowInsecureProtocol = System.getenv().containsKey("NEXUS_INSECURE").takeUnless { it == false } ?: extra["NEXUS_INSECURE"].toString().toBoolean()
+            credentials {
+                username = System.getenv("NEXUS_USERNAME").takeUnless { it.isNullOrEmpty() } ?: extra["NEXUS_USERNAME"].toString()
+                password = System.getenv("NEXUS_PASSWORD").takeUnless { it.isNullOrEmpty() } ?: extra["NEXUS_PASSWORD"].toString()
+            }
+        }
+    }
+}
+
 repositories { mavenCentral() }
 
 java { toolchain { languageVersion.set(JavaLanguageVersion.of(17)) } }
@@ -64,34 +96,5 @@ allprojects {
     }
   }
 
-    publishing {
-      publications {
-        create<MavenPublication>("maven") {
-          from(components["java"])
-        }
-      }
-      repositories {
-        maven {
-          name = "GITHUB"
-          val releasesRepoUrl = System.getenv("GITHUB_URL").takeUnless { it.isNullOrEmpty() } ?: extra["GITHUB_URL"].toString()
-          url = uri(releasesRepoUrl)
-          credentials {
-            username = System.getenv("GITHUB_ACTOR").takeUnless { it.isNullOrEmpty() } ?: extra["GITHUB_ACTOR"].toString()
-            password = System.getenv("GITHUB_TOKEN").takeUnless { it.isNullOrEmpty() } ?: extra["GITHUB_TOKEN"].toString()
-          }
-        }
-        maven {
-          name = "NEXUS"
-          val nexusUrl = System.getenv("NEXUS_URL").takeUnless { it.isNullOrEmpty() } ?: extra["NEXUS_URL"].toString()
-          val releasesRepoUrl = nexusUrl + "/maven-releases"
-          val snapshotsRepoUrl = nexusUrl + "/maven-snapshots"
-          url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-          isAllowInsecureProtocol = System.getenv().containsKey("NEXUS_INSECURE").takeUnless { it == false } ?: extra["NEXUS_INSECURE"].toString().toBoolean()
-          credentials {
-            username = System.getenv("NEXUS_USERNAME").takeUnless { it.isNullOrEmpty() } ?: extra["NEXUS_USERNAME"].toString()
-            password = System.getenv("NEXUS_PASSWORD").takeUnless { it.isNullOrEmpty() } ?: extra["NEXUS_PASSWORD"].toString()
-          }
-        }
-      }
-    }
+
 }
