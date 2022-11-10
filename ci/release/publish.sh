bea769b7c5e8b4e6494717ba7589065fd45f5dc4
david dali susanibar arce <davi.sarces@gmail.com>
2022-11-09 23:30:12 -0500

    build: delete duplicate close sonatype

--- ci/release/publish.sh
+++ ci/release/publish.sh
@@ -4,4 +4,4 @@
 set -euo pipefail
 
 gradle wrapper
-./gradlew clean :core:publishToSonatype :isthmus:publishToSonatype closeSonatypeStagingRepository closeSonatypeStagingRepository
+./gradlew clean :core:publishToSonatype :isthmus:publishToSonatype closeSonatypeStagingRepository
