3542a605b860865e28b99e7b745cdd2e5f69380f
Victor Barua <victor.barua@datadoghq.com>
2023-02-16 14:37:22 -0800

    refactor: simplify arithmetic tests

--- isthmus/src/test/java/io/substrait/isthmus/ArithmeticFunctionTest.java
+++ isthmus/src/test/java/io/substrait/isthmus/ArithmeticFunctionTest.java
@@ -1,7 +1,6 @@
 package io.substrait.isthmus;
 
 import java.util.List;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,70 +8,63 @@ public class ArithmeticFunctionTest extends PlanTestBase {
 
   static List<String> CREATES =
       List.of(
-          "CREATE TABLE INTS (i8 TINYINT, i16 SMALLINT, i32 INT, i64 BIGINT)",
-          "CREATE TABLE FLOATS (fp32 FLOAT, fp64 DOUBLE)");
+          "CREATE TABLE numbers (i8 TINYINT, i16 SMALLINT, i32 INT, i64 BIGINT, fp32 FLOAT, fp64 DOUBLE)");
 
   @ParameterizedTest
-  @ValueSource(strings = {"i8", "i16", "i32", "i64"})
-  void integerArithmetic(String c) throws Exception {
+  @ValueSource(strings = {"i8", "i16", "i32", "i64", "fp32", "fp64"})
+  void arithmetic(String c) throws Exception {
     String query =
         String.format(
-            "SELECT %s + %s, %s - %s, %s * %s, %s / %s FROM ints", c, c, c, c, c, c, c, c);
+            "SELECT %s + %s, %s - %s, %s * %s, %s / %s FROM numbers", c, c, c, c, c, c, c, c);
+    assertSqlSubstraitRelRoundTrip(query, CREATES);
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"i8", "i16", "i32", "i64", "fp32", "fp64"})
+  void abs(String column) throws Exception {
+    String query = String.format("SELECT abs(%s) FROM numbers", column);
     assertSqlSubstraitRelRoundTrip(query, CREATES);
   }
 
   @ParameterizedTest
   @ValueSource(strings = {"fp32", "fp64"})
-  void floatingPointArithmetic(String c) throws Exception {
-    String query =
-        String.format(
-            "SELECT %s + %s, %s - %s, %s * %s, %s / %s FROM floats", c, c, c, c, c, c, c, c);
+  void exponential(String column) throws Exception {
+    String query = String.format("SELECT exp(%s) FROM numbers", column);
     assertSqlSubstraitRelRoundTrip(query, CREATES);
-  }
-
-  @Test
-  void abs() throws Exception {
-    assertSqlSubstraitRelRoundTrip(
-        "SELECT abs(i8), abs(i16), abs(i32), abs(i64) FROM ints", CREATES);
-    assertSqlSubstraitRelRoundTrip("SELECT abs(fp32), abs(fp64) FROM floats", CREATES);
-  }
-
-  @Test
-  void exponential() throws Exception {
-    assertSqlSubstraitRelRoundTrip("SELECT exp(fp32), exp(fp64) FROM floats", CREATES);
   }
 
   @ParameterizedTest
   @ValueSource(strings = {"i8", "i16", "i32", "i64"})
-  void mod(String c) throws Exception {
-    String query = String.format("SELECT mod(%s, %s) FROM ints", c, c);
+  void mod(String column) throws Exception {
+    String query = String.format("SELECT mod(%s, %s) FROM numbers", column, column);
     assertSqlSubstraitRelRoundTrip(query, CREATES);
   }
 
-  @Test
-  void negation() throws Exception {
-    assertSqlSubstraitRelRoundTrip("SELECT -i8, -i16, -i32, -i64 FROM ints", CREATES);
-    assertSqlSubstraitRelRoundTrip("SELECT -fp32, -fp64 FROM floats", CREATES);
+  @ParameterizedTest
+  @ValueSource(strings = {"i8", "i16", "i32", "i64", "fp32", "fp64"})
+  void negation(String column) throws Exception {
+    String query = String.format("SELECT -%s FROM numbers", column);
+    assertSqlSubstraitRelRoundTrip(query, CREATES);
   }
 
-  @Test
-  void power() throws Exception {
-    assertSqlSubstraitRelRoundTrip("SELECT  power(i64, i64) FROM ints", CREATES);
-    assertSqlSubstraitRelRoundTrip(
-        "SELECT power(fp32, fp32), power(fp64, fp64) FROM floats", CREATES);
+  @ParameterizedTest
+  @ValueSource(strings = {"i64", "fp32", "fp64"})
+  void power(String column) throws Exception {
+    String query = String.format("SELECT power(%s, %s) FROM numbers", column, column);
+    assertSqlSubstraitRelRoundTrip(query, CREATES);
   }
 
   @ParameterizedTest
   @ValueSource(strings = {"sin", "cos", "tan", "asin", "acos", "atan"})
   void trigonometric(String fname) throws Exception {
-    String query = String.format("SELECT %s(fp32), %s(fp64) FROM floats", fname, fname);
+    String query = String.format("SELECT %s(fp32), %s(fp64) FROM numbers", fname, fname);
     assertSqlSubstraitRelRoundTrip(query, CREATES);
   }
 
-  @Test
-  void sign() throws Exception {
-    assertSqlSubstraitRelRoundTrip(
-        "SELECT sign(i8), sign(i16), sign(i32), sign(i64) FROM ints", CREATES);
-    assertSqlSubstraitRelRoundTrip("SELECT sign(fp32), sign(fp64) FROM floats", CREATES);
+  @ParameterizedTest
+  @ValueSource(strings = {"i8", "i16", "i32", "i64", "fp32", "fp64"})
+  void sign(String column) throws Exception {
+    String query = String.format("SELECT sign(%s) FROM numbers", column);
+    assertSqlSubstraitRelRoundTrip(query, CREATES);
   }
 }
