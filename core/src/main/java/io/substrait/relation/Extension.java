4860b57e94c0bd53dd1aeb9a15f0547ad1f4553d
Victor Barua <victor.barua@datadoghq.com>
2023-04-05 10:34:27 -0700

    refactor: extract ToProto interface

--- core/src/main/java/io/substrait/relation/Extension.java
+++ core/src/main/java/io/substrait/relation/Extension.java
@@ -7,33 +7,29 @@ import java.util.List;
 /** Contains tag interfaces for handling {@link com.google.protobuf.Any} types within Substrait. */
 public class Extension {
 
-  private interface ToProto {
-    com.google.protobuf.Any toProto();
-  }
-
   /**
    * Optimization associated with an {@link io.substrait.proto.AdvancedExtension}
    *
    * <p>An optimization is helpful information that don't influence semantics. May be ignored by a
    * consumer.
    */
-  public interface Optimization extends ToProto {}
+  public interface Optimization extends ToProto<com.google.protobuf.Any> {}
 
   /**
    * Enhancement associated with an {@link io.substrait.proto.AdvancedExtension}
    *
    * <p>An enhancement alter semantics. Cannot be ignored by a consumer.
    */
-  public interface Enhancement extends ToProto {}
+  public interface Enhancement extends ToProto<com.google.protobuf.Any> {}
 
-  public interface LeafRelDetail extends ToProto {
+  public interface LeafRelDetail extends ToProto<com.google.protobuf.Any> {
     /**
      * @return the record layout for the associated {@link ExtensionLeaf} relation
      */
     Type.Struct deriveRecordType();
   }
 
-  public interface SingleRelDetail extends ToProto {
+  public interface SingleRelDetail extends ToProto<com.google.protobuf.Any> {
     /**
      * @param input to the associated {@link ExtensionSingle} relation
      * @return the record layout for the associated {@link ExtensionSingle} relation
@@ -41,7 +37,7 @@ public class Extension {
     Type.Struct deriveRecordType(Rel input);
   }
 
-  public interface MultiRelDetail extends ToProto {
+  public interface MultiRelDetail extends ToProto<com.google.protobuf.Any> {
     /**
      * @param inputs to the associated {@link ExtensionMulti} relation
      * @return the record layout for the associated {@link ExtensionMulti} relation
@@ -49,7 +45,7 @@ public class Extension {
     Type.Struct deriveRecordType(List<Rel> inputs);
   }
 
-  public interface ExtensionTableDetail extends ToProto {
+  public interface ExtensionTableDetail extends ToProto<com.google.protobuf.Any> {
     /**
      * @return the table schema for the associated {@link ExtensionTable} relation
      */
