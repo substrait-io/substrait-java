4769d2f7320c55e1d84403f095bad7c032121fa9
Victor Barua <victor.barua@datadoghq.com>
2023-11-02 15:57:32 -0700

    test: move common logic into TestBase (#193)

--- core/src/test/java/io/substrait/type/proto/LiteralRoundtripTest.java
+++ core/src/test/java/io/substrait/type/proto/LiteralRoundtripTest.java
@@ -7,25 +7,16 @@ import io.substrait.TestBase;
 import io.substrait.expression.ExpressionCreator;
 import io.substrait.expression.proto.ExpressionProtoConverter;
 import io.substrait.expression.proto.ProtoExpressionConverter;
-import io.substrait.extension.ExtensionCollector;
-import io.substrait.relation.ProtoRelConverter;
 import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 
 public class LiteralRoundtripTest extends TestBase {
-  static final org.slf4j.Logger logger =
-      org.slf4j.LoggerFactory.getLogger(LiteralRoundtripTest.class);
 
   @Test
   void decimal() {
     var val = ExpressionCreator.decimal(false, BigDecimal.TEN, 10, 2);
     var to = new ExpressionProtoConverter(null, null);
-    var from =
-        new ProtoExpressionConverter(
-            null,
-            null,
-            EMPTY_TYPE,
-            new ProtoRelConverter(new ExtensionCollector(), defaultExtensionCollection));
+    var from = new ProtoExpressionConverter(null, null, EMPTY_TYPE, protoRelConverter);
     assertEquals(val, from.from(val.accept(to)));
   }
 }
