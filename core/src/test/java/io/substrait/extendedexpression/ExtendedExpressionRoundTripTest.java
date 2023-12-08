c27dd372f93b6462c98e141fa9f9c176dfbbf906
david dali susanibar arce <davi.sarces@gmail.com>
2023-12-08 15:56:23 -0500

    fix: clean code

--- core/src/test/java/io/substrait/extendedexpression/ExtendedExpressionRoundTripTest.java
+++ core/src/test/java/io/substrait/extendedexpression/ExtendedExpressionRoundTripTest.java
@@ -3,7 +3,6 @@ package io.substrait.extendedexpression;
 import io.substrait.TestBase;
 import io.substrait.expression.*;
 import io.substrait.relation.Aggregate;
-import io.substrait.relation.ImmutableMeasure;
 import io.substrait.type.ImmutableNamedStruct;
 import io.substrait.type.Type;
 import io.substrait.type.TypeCreator;
@@ -20,118 +19,56 @@ public class ExtendedExpressionRoundTripTest extends TestBase {
   @Test
   public void expressionRoundTrip() throws IOException {
     // create predefined POJO extended expression
-    Expression.ScalarFunctionInvocation scalarFunctionInvocation =
-        b.scalarFn(
-            NAMESPACE,
-            "add:dec_dec",
-            TypeCreator.REQUIRED.BOOLEAN,
-            ImmutableFieldReference.builder()
-                .addSegments(FieldReference.StructField.of(0))
-                .type(TypeCreator.REQUIRED.decimal(10, 2))
-                .build(),
-            ExpressionCreator.i32(false, 183));
+    ImmutableExpressionReference expressionReference = getImmutableExpressionReference();
 
-    ImmutableExpressionReference expressionReference =
-        ImmutableExpressionReference.builder()
-            .expression(scalarFunctionInvocation)
-            .addOutputNames("new-column")
-            .build();
-
-    List<io.substrait.extendedexpression.ExtendedExpression.ExpressionReference>
-        expressionReferences = new ArrayList<>();
+    List<ExtendedExpression.ExpressionReferenceBase> expressionReferences = new ArrayList<>();
+    // adding expression
     expressionReferences.add(expressionReference);
 
-    ImmutableNamedStruct namedStruct =
-        ImmutableNamedStruct.builder()
-            .addNames("N_NATIONKEY", "N_NAME", "N_REGIONKEY", "N_COMMENT")
-            .struct(
-                Type.Struct.builder()
-                    .nullable(false)
-                    .addFields(
-                        TypeCreator.REQUIRED.decimal(10, 2),
-                        TypeCreator.REQUIRED.STRING,
-                        TypeCreator.REQUIRED.decimal(10, 2),
-                        TypeCreator.REQUIRED.STRING)
-                    .build())
-            .build();
+    ImmutableNamedStruct namedStruct = getImmutableNamedStruct();
 
-    // pojo initial extended expression
-    io.substrait.extendedexpression.ExtendedExpression extendedExpressionPojoInitial =
-        ImmutableExtendedExpression.builder()
-            .referredExpressions(expressionReferences)
-            .baseSchema(namedStruct)
-            .build();
-
-    // proto extended expression
-    io.substrait.proto.ExtendedExpression extendedExpressionProto =
-        new ExtendedExpressionProtoConverter().toProto(extendedExpressionPojoInitial);
-
-    // pojo final extended expression
-    io.substrait.extendedexpression.ExtendedExpression extendedExpressionPojoFinal =
-        new ProtoExtendedExpressionConverter().from(extendedExpressionProto);
-
-    // validate extended expression pojo initial equals to final roundtrip
-    Assertions.assertEquals(extendedExpressionPojoInitial, extendedExpressionPojoFinal);
+    assertExtendedExpressionOperation(expressionReferences, namedStruct);
   }
 
   @Test
   public void aggregationRoundTrip() throws IOException {
     // create predefined POJO aggregation function
-    io.substrait.relation.Aggregate.Measure measure =
-        Aggregate.Measure.builder()
-            .function(
-                AggregateFunctionInvocation.builder()
-                    .arguments(Collections.emptyList())
-                    .declaration(defaultExtensionCollection.aggregateFunctions().get(0))
-                    .outputType(TypeCreator.of(false).I64)
-                    .aggregationPhase(Expression.AggregationPhase.INITIAL_TO_RESULT)
-                    .invocation(Expression.AggregationInvocation.ALL)
-                    .build())
-            .build();
-
     ImmutableAggregateFunctionReference aggregateFunctionReference =
-        ImmutableAggregateFunctionReference.builder().measure(measure).build();
+        getImmutableAggregateFunctionReference();
 
     List<ExtendedExpression.ExpressionReferenceBase> expressionReferences = new ArrayList<>();
+    // adding aggregation function
     expressionReferences.add(aggregateFunctionReference);
 
-    ImmutableNamedStruct namedStruct =
-        ImmutableNamedStruct.builder()
-            .addNames("N_NATIONKEY", "N_NAME", "N_REGIONKEY", "N_COMMENT")
-            .struct(
-                Type.Struct.builder()
-                    .nullable(false)
-                    .addFields(
-                        TypeCreator.REQUIRED.decimal(10, 2),
-                        TypeCreator.REQUIRED.STRING,
-                        TypeCreator.REQUIRED.decimal(10, 2),
-                        TypeCreator.REQUIRED.STRING)
-                    .build())
-            .build();
+    ImmutableNamedStruct namedStruct = getImmutableNamedStruct();
 
-    // pojo initial aggregation function
-    io.substrait.extendedexpression.ExtendedExpression extendedExpressionPojoInitial =
-        ImmutableExtendedExpression.builder()
-            .referredExpressions(expressionReferences)
-            .baseSchema(namedStruct)
-            .build();
-
-    // proto aggregation function
-    io.substrait.proto.ExtendedExpression extendedExpressionProto =
-        new ExtendedExpressionProtoConverter().toProto(extendedExpressionPojoInitial);
-
-    // pojo final aggregation function
-    io.substrait.extendedexpression.ExtendedExpression extendedExpressionPojoFinal =
-        new ProtoExtendedExpressionConverter().from(extendedExpressionProto);
-
-    // validate aggregation function pojo initial equals to final roundtrip
-    Assertions.assertEquals(extendedExpressionPojoInitial, extendedExpressionPojoFinal);
+    assertExtendedExpressionOperation(expressionReferences, namedStruct);
   }
 
   @Test
   public void expressionAndAggregationRoundTrip() throws IOException {
     // POJO 01
     // create predefined POJO extended expression
+    ImmutableExpressionReference expressionReference = getImmutableExpressionReference();
+
+    List<ExtendedExpression.ExpressionReferenceBase> expressionReferences = new ArrayList<>();
+
+    // POJO 02
+    // create predefined POJO aggregation function
+    ImmutableAggregateFunctionReference aggregateFunctionReference =
+        getImmutableAggregateFunctionReference();
+
+    // adding expression
+    expressionReferences.add(expressionReference);
+    // adding aggregation function
+    expressionReferences.add(aggregateFunctionReference);
+
+    ImmutableNamedStruct namedStruct = getImmutableNamedStruct();
+
+    assertExtendedExpressionOperation(expressionReferences, namedStruct);
+  }
+
+  private ImmutableExpressionReference getImmutableExpressionReference() {
     Expression.ScalarFunctionInvocation scalarFunctionInvocation =
         b.scalarFn(
             NAMESPACE,
@@ -143,17 +80,14 @@ public class ExtendedExpressionRoundTripTest extends TestBase {
                 .build(),
             ExpressionCreator.i32(false, 183));
 
-    ImmutableExpressionReference expressionReference =
-        ImmutableExpressionReference.builder()
-            .expression(scalarFunctionInvocation)
-            .addOutputNames("new-column")
-            .build();
+    return ImmutableExpressionReference.builder()
+        .expression(scalarFunctionInvocation)
+        .addOutputNames("new-column")
+        .build();
+  }
 
-    List<ExtendedExpression.ExpressionReferenceBase> expressionReferences = new ArrayList<>();
-
-    // POJO 02
-    // create predefined POJO aggregation function
-    ImmutableMeasure measure =
+  private static ImmutableAggregateFunctionReference getImmutableAggregateFunctionReference() {
+    Aggregate.Measure measure =
         Aggregate.Measure.builder()
             .function(
                 AggregateFunctionInvocation.builder()
@@ -165,47 +99,47 @@ public class ExtendedExpressionRoundTripTest extends TestBase {
                     .build())
             .build();
 
-    ImmutableAggregateFunctionReference aggregateFunctionReference =
-        ImmutableAggregateFunctionReference.builder()
-            .measure(measure)
-            .addOutputNames("new-column")
-            .build();
+    return ImmutableAggregateFunctionReference.builder()
+        .measure(measure)
+        .addOutputNames("new-column")
+        .build();
+  }
 
-    // adding expression
-    expressionReferences.add(expressionReference);
-    // adding aggregation function
-    expressionReferences.add(aggregateFunctionReference);
+  private static ImmutableNamedStruct getImmutableNamedStruct() {
+    return ImmutableNamedStruct.builder()
+        .addNames("N_NATIONKEY", "N_NAME", "N_REGIONKEY", "N_COMMENT")
+        .struct(
+            Type.Struct.builder()
+                .nullable(false)
+                .addFields(
+                    TypeCreator.REQUIRED.decimal(10, 2),
+                    TypeCreator.REQUIRED.STRING,
+                    TypeCreator.REQUIRED.decimal(10, 2),
+                    TypeCreator.REQUIRED.STRING)
+                .build())
+        .build();
+  }
 
-    ImmutableNamedStruct namedStruct =
-        ImmutableNamedStruct.builder()
-            .addNames("N_NATIONKEY", "N_NAME", "N_REGIONKEY", "N_COMMENT")
-            .struct(
-                Type.Struct.builder()
-                    .nullable(false)
-                    .addFields(
-                        TypeCreator.REQUIRED.decimal(10, 2),
-                        TypeCreator.REQUIRED.STRING,
-                        TypeCreator.REQUIRED.decimal(10, 2),
-                        TypeCreator.REQUIRED.STRING)
-                    .build())
-            .build();
+  private static void assertExtendedExpressionOperation(
+      List<ExtendedExpression.ExpressionReferenceBase> expressionReferences,
+      ImmutableNamedStruct namedStruct)
+      throws IOException {
 
-    // pojo initial extended expression + aggregation
-    io.substrait.extendedexpression.ExtendedExpression extendedExpressionPojoInitial =
+    // initial pojo
+    ExtendedExpression extendedExpressionPojoInitial =
         ImmutableExtendedExpression.builder()
             .referredExpressions(expressionReferences)
             .baseSchema(namedStruct)
             .build();
 
-    // proto extended expression + aggregation
+    // proto
     io.substrait.proto.ExtendedExpression extendedExpressionProto =
         new ExtendedExpressionProtoConverter().toProto(extendedExpressionPojoInitial);
 
-    // pojo final extended expression + aggregation
-    io.substrait.extendedexpression.ExtendedExpression extendedExpressionPojoFinal =
+    // get pojo from proto
+    ExtendedExpression extendedExpressionPojoFinal =
         new ProtoExtendedExpressionConverter().from(extendedExpressionProto);
 
-    // validate extended expression + aggregation pojo initial equals to final roundtrip
     Assertions.assertEquals(extendedExpressionPojoInitial, extendedExpressionPojoFinal);
   }
 }
