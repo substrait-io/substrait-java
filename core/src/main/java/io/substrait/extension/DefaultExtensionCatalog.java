e362a3169b0dea9f279eecdf788d928329a194c5
Victor Barua <victor.barua@datadoghq.com>
2024-01-03 14:22:43 -0800

    feat: introduce DefaultExtensionCatalog (#217)
    contains static strings for default extensions

---
+++ core/src/main/java/io/substrait/extension/DefaultExtensionCatalog.java
@@ -0,0 +1,16 @@
+package io.substrait.extension;
+
+public class DefaultExtensionCatalog {
+  public static final String FUNCTIONS_AGGREGATE_APPROX = "/functions_aggregate_approx.yaml";
+  public static final String FUNCTIONS_AGGREGATE_GENERIC = "/functions_aggregate_generic.yaml";
+  public static final String FUNCTIONS_ARITHMETIC = "/functions_arithmetic.yaml";
+  public static final String FUNCTIONS_ARITHMETIC_DECIMAL = "/functions_arithmetic_decimal.yaml";
+  public static final String FUNCTIONS_BOOLEAN = "/functions_boolean.yaml";
+  public static final String FUNCTIONS_COMPARISON = "/functions_comparison.yaml";
+  public static final String FUNCTIONS_DATETIME = "/functions_datetime.yaml";
+  public static final String FUNCTIONS_GEOMETRY = "/functions_geometry.yaml";
+  public static final String FUNCTIONS_LOGARITHMIC = "/functions_logarithmic.yaml";
+  public static final String FUNCTIONS_ROUNDING = "/functions_rounding.yaml";
+  public static final String FUNCTIONS_SET = "/functions_set.yaml";
+  public static final String FUNCTIONS_STRING = "/functions_string.yaml";
+}
--- core/src/main/java/io/substrait/type/YamlRead.java
+++ core/src/main/java/io/substrait/type/YamlRead.java
@@ -3,6 +3,7 @@ package io.substrait.type;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import io.substrait.extension.DefaultExtensionCatalog;
 import io.substrait.extension.SimpleExtension;
 import java.io.File;
 import java.util.*;
@@ -25,7 +26,8 @@ public class YamlRead {
 
   public static void main(String[] args) throws Exception {
     try {
-      System.out.println("Read: " + YamlRead.class.getResource("/functions_boolean.yaml"));
+      System.out.println(
+          "Read: " + YamlRead.class.getResource(DefaultExtensionCatalog.FUNCTIONS_BOOLEAN));
       List<SimpleExtension.Function> signatures = loadFunctions();
 
       signatures.forEach(f -> System.out.println(f.key()));
