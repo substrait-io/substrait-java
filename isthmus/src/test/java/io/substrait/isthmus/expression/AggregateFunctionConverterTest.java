0bdac4949b26896e4c7da50b6362debacee365a1
Bruno Volpato <bruno.volpato@datadoghq.com>
2024-02-21 20:05:58 -0500

    fix(isthmus): allow for conversion of plans containing Calcite SqlAggFunctions (#230)
    Calcite planning rules can introduce the Calcite variants of SqlAggFunctions
    
    In #180, Substrait specific variants for these function were introduced which
    better matched the type inference for these functions as defined in Substrait
    
    Those changes cause failures when converting the Calcite variants to Substrait,
    which is what these changes address
    
    ---------
    
    Co-authored-by: Victor Barua <victor.barua@datadoghq.com>

---
+++ isthmus/src/test/java/io/substrait/isthmus/expression/AggregateFunctionConverterTest.java
@@ -0,0 +1,35 @@
+package io.substrait.isthmus.expression;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.substrait.isthmus.AggregateFunctions;
+import io.substrait.isthmus.PlanTestBase;
+import io.substrait.isthmus.TypeConverter;
+import java.util.List;
+import org.apache.calcite.rel.core.AggregateCall;
+import org.apache.calcite.sql.fun.SqlSumEmptyIsZeroAggFunction;
+import org.apache.calcite.sql.type.SqlTypeName;
+import org.junit.jupiter.api.Test;
+
+public class AggregateFunctionConverterTest extends PlanTestBase {
+
+  @Test
+  void testFunctionFinderMatch() {
+    AggregateFunctionConverter converter =
+        new AggregateFunctionConverter(
+            extensions.aggregateFunctions(), List.of(), typeFactory, TypeConverter.DEFAULT);
+
+    var functionFinder =
+        converter.getFunctionFinder(
+            AggregateCall.create(
+                new SqlSumEmptyIsZeroAggFunction(),
+                true,
+                List.of(1),
+                0,
+                typeFactory.createSqlType(SqlTypeName.VARCHAR),
+                null));
+    assertNotNull(functionFinder);
+    assertEquals("sum0", functionFinder.getName());
+    assertEquals(AggregateFunctions.SUM0, functionFinder.getOperator());
+  }
+}
