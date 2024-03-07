79c7299722a4e2e8e8d526bc3f9d37e41d393480
Victor Barua <victor.barua@datadoghq.com>
2024-03-07 15:12:55 -0800

    build: extract out initializeAtBuildTime

--- isthmus/build.gradle.kts
+++ isthmus/build.gradle.kts
@@ -103,15 +103,92 @@ dependencies {
   compileOnly("com.github.bsideup.jabel:jabel-javac-plugin:0.4.2")
 }
 
+var initializeAtBuildTime =
+  listOf(
+      "com.google.common.base.Platform",
+      "com.google.common.base.Preconditions",
+      "com.google.common.cache.CacheBuilder",
+      "com.google.common.cache.LocalCache",
+      "com.google.common.collect.ImmutableRangeSet",
+      "com.google.common.collect.ImmutableSortedMap",
+      "com.google.common.collect.Platform",
+      "com.google.common.collect.Range",
+      "com.google.common.collect.RegularImmutableMap",
+      "com.google.common.collect.RegularImmutableSortedSet",
+      "com.google.common.math.IntMath",
+      "com.google.common.math.IntMath\$1",
+      "com.google.common.primitives.Primitives",
+      "com.google.common.util.concurrent.AbstractFuture",
+      "com.google.common.util.concurrent.AbstractFuture",
+      "com.google.common.util.concurrent.AbstractFuture\$UnsafeAtomicHelper",
+      "com.google.common.util.concurrent.SettableFuture",
+      "io.substrait.isthmus.InitializeAtBuildTime",
+      "io.substrait.isthmus.metadata.LambdaHandlerCache",
+      "io.substrait.isthmus.metadata.LambdaMetadataSupplier",
+      "io.substrait.isthmus.metadata.LegacyToLambdaGenerator",
+      "org.apache.calcite.config.CalciteSystemProperty",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$AllPredicates",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$Collation",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$ColumnOrigin",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$ColumnUniqueness",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$CumulativeCost",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$DistinctRowCount",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$Distribution",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$ExplainVisibility",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$ExpressionLineage",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$LowerBoundCost",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$MaxRowCount",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$Memory",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$MinRowCount",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$NodeTypes",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$NonCumulativeCost",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$Parallelism",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$PercentageOriginalRows",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$PopulationSize",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$Predicates",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$RowCount",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$Selectivity",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$Size",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$TableReferences",
+      "org.apache.calcite.rel.metadata.BuiltInMetadata\$UniqueKeys",
+      "org.apache.calcite.rel.metadata.RelMdAllPredicates",
+      "org.apache.calcite.rel.metadata.RelMdCollation",
+      "org.apache.calcite.rel.metadata.RelMdColumnOrigins",
+      "org.apache.calcite.rel.metadata.RelMdColumnUniqueness",
+      "org.apache.calcite.rel.metadata.RelMdDistinctRowCount",
+      "org.apache.calcite.rel.metadata.RelMdDistribution",
+      "org.apache.calcite.rel.metadata.RelMdExplainVisibility",
+      "org.apache.calcite.rel.metadata.RelMdExpressionLineage",
+      "org.apache.calcite.rel.metadata.RelMdLowerBoundCost",
+      "org.apache.calcite.rel.metadata.RelMdMaxRowCount",
+      "org.apache.calcite.rel.metadata.RelMdMemory",
+      "org.apache.calcite.rel.metadata.RelMdMinRowCount",
+      "org.apache.calcite.rel.metadata.RelMdNodeTypes",
+      "org.apache.calcite.rel.metadata.RelMdParallelism",
+      "org.apache.calcite.rel.metadata.RelMdPercentageOriginalRows",
+      "org.apache.calcite.rel.metadata.RelMdPopulationSize",
+      "org.apache.calcite.rel.metadata.RelMdPredicates",
+      "org.apache.calcite.rel.metadata.RelMdRowCount",
+      "org.apache.calcite.rel.metadata.RelMdSelectivity",
+      "org.apache.calcite.rel.metadata.RelMdSize",
+      "org.apache.calcite.rel.metadata.RelMdTableReferences",
+      "org.apache.calcite.rel.metadata.RelMdUniqueKeys",
+      "org.apache.calcite.util.Pair",
+      "org.apache.calcite.util.Util",
+      "org.apache.commons.codec.language.Soundex",
+      "org.slf4j.LoggerFactory",
+      "org.slf4j.impl.JDK14LoggerAdapter",
+      "org.slf4j.impl.StaticLoggerBinder",
+    )
+    .joinToString(",")
+
 graal {
   mainClass("io.substrait.isthmus.IsthmusEntryPoint")
   outputName("isthmus")
   graalVersion("22.1.0")
   javaVersion("17")
   option("--no-fallback")
-  option(
-    "--initialize-at-build-time=io.substrait.isthmus.InitializeAtBuildTime,org.slf4j.impl.StaticLoggerBinder,com.google.common.math.IntMath\$1,com.google.common.base.Platform,com.google.common.util.concurrent.AbstractFuture\$UnsafeAtomicHelper,com.google.common.collect.ImmutableSortedMap,com.google.common.math.IntMath,com.google.common.collect.RegularImmutableSortedSet,com.google.common.cache.LocalCache,com.google.common.collect.Range,org.apache.commons.codec.language.Soundex,com.google.common.collect.ImmutableRangeSet,org.slf4j.LoggerFactory,com.google.common.collect.Platform,com.google.common.util.concurrent.SettableFuture,com.google.common.util.concurrent.AbstractFuture,com.google.common.util.concurrent.AbstractFuture,com.google.common.cache.CacheBuilder,com.google.common.base.Preconditions,com.google.common.collect.RegularImmutableMap,org.slf4j.impl.JDK14LoggerAdapter,org.apache.calcite.rel.metadata.RelMdColumnUniqueness,org.apache.calcite.rel.metadata.BuiltInMetadata\$ColumnOrigin,io.substrait.isthmus.metadata.LambdaMetadataSupplier,org.apache.calcite.rel.metadata.BuiltInMetadata\$PopulationSize,org.apache.calcite.rel.metadata.BuiltInMetadata\$Size,org.apache.calcite.rel.metadata.BuiltInMetadata\$UniqueKeys,org.apache.calcite.rel.metadata.RelMdColumnOrigins,org.apache.calcite.rel.metadata.RelMdExplainVisibility,org.apache.calcite.rel.metadata.RelMdMemory,org.apache.calcite.rel.metadata.RelMdExpressionLineage,org.apache.calcite.rel.metadata.RelMdDistinctRowCount,org.apache.calcite.rel.metadata.BuiltInMetadata\$RowCount,org.apache.calcite.rel.metadata.BuiltInMetadata\$PercentageOriginalRows,org.apache.calcite.util.Pair,org.apache.calcite.rel.metadata.BuiltInMetadata\$ExpressionLineage,org.apache.calcite.rel.metadata.BuiltInMetadata\$MinRowCount,com.google.common.primitives.Primitives,org.apache.calcite.rel.metadata.BuiltInMetadata\$Selectivity,org.apache.calcite.rel.metadata.BuiltInMetadata\$Parallelism,org.apache.calcite.rel.metadata.RelMdUniqueKeys,org.apache.calcite.rel.metadata.RelMdParallelism,org.apache.calcite.rel.metadata.RelMdPercentageOriginalRows,org.apache.calcite.rel.metadata.BuiltInMetadata\$Predicates,org.apache.calcite.rel.metadata.BuiltInMetadata\$Distribution,org.apache.calcite.config.CalciteSystemProperty,org.apache.calcite.rel.metadata.BuiltInMetadata\$NonCumulativeCost,org.apache.calcite.util.Util,org.apache.calcite.rel.metadata.RelMdAllPredicates,io.substrait.isthmus.metadata.LambdaHandlerCache,org.apache.calcite.rel.metadata.BuiltInMetadata\$TableReferences,org.apache.calcite.rel.metadata.RelMdNodeTypes,org.apache.calcite.rel.metadata.RelMdCollation,org.apache.calcite.rel.metadata.RelMdSelectivity,org.apache.calcite.rel.metadata.BuiltInMetadata\$NodeTypes,org.apache.calcite.rel.metadata.RelMdPredicates,org.apache.calcite.rel.metadata.BuiltInMetadata\$DistinctRowCount,org.apache.calcite.rel.metadata.RelMdRowCount,org.apache.calcite.rel.metadata.BuiltInMetadata\$MaxRowCount,org.apache.calcite.rel.metadata.BuiltInMetadata\$AllPredicates,org.apache.calcite.rel.metadata.RelMdMaxRowCount,org.apache.calcite.rel.metadata.RelMdLowerBoundCost,org.apache.calcite.rel.metadata.BuiltInMetadata\$ExplainVisibility,org.apache.calcite.rel.metadata.BuiltInMetadata\$ColumnUniqueness,org.apache.calcite.rel.metadata.RelMdPopulationSize,org.apache.calcite.rel.metadata.BuiltInMetadata\$Memory,org.apache.calcite.rel.metadata.RelMdMinRowCount,org.apache.calcite.rel.metadata.RelMdSize,org.apache.calcite.rel.metadata.BuiltInMetadata\$LowerBoundCost,org.apache.calcite.rel.metadata.RelMdTableReferences,org.apache.calcite.rel.metadata.RelMdDistribution,io.substrait.isthmus.metadata.LegacyToLambdaGenerator,org.apache.calcite.rel.metadata.BuiltInMetadata\$CumulativeCost,org.apache.calcite.rel.metadata.BuiltInMetadata\$Collation"
-  )
+  option("--initialize-at-build-time=${initializeAtBuildTime}")
   option("-H:IncludeResources=.*yaml")
   option("--report-unsupported-elements-at-runtime")
   option("-H:+ReportExceptionStackTraces")
