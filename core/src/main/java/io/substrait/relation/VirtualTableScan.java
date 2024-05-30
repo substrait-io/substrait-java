3bbcf82687bc51fdb1695436c198e91ba56befed
Arttu <Blizzara@users.noreply.github.com>
2024-05-30 21:56:54 +0200

    fix: account for struct fields in VirtualTableScan check (#255)

--- core/src/main/java/io/substrait/relation/VirtualTableScan.java
+++ core/src/main/java/io/substrait/relation/VirtualTableScan.java
@@ -3,6 +3,7 @@ package io.substrait.relation;
 import io.substrait.expression.Expression;
 import io.substrait.type.NamedStruct;
 import io.substrait.type.Type;
+import io.substrait.type.TypeVisitor;
 import java.util.List;
 import org.immutables.value.Value;
 
@@ -30,7 +31,9 @@ public abstract class VirtualTableScan extends AbstractReadRel {
 
     assert rows.size() > 0
         && names.stream().noneMatch(s -> s == null)
-        && rows.stream().noneMatch(r -> r == null || r.fields().size() != names.size());
+        && rows.stream().noneMatch(r -> r == null)
+        && rows.stream()
+            .allMatch(r -> r.getType().accept(new NamedFieldCountingTypeVisitor()) == names.size());
   }
 
   @Override
@@ -45,5 +48,140 @@ public abstract class VirtualTableScan extends AbstractReadRel {
 
   public static ImmutableVirtualTableScan.Builder builder() {
     return ImmutableVirtualTableScan.builder();
+  }
+
+  private static class NamedFieldCountingTypeVisitor
+      implements TypeVisitor<Integer, RuntimeException> {
+    @Override
+    public Integer visit(Type.Bool type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.I8 type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.I16 type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.I32 type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.I64 type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.FP32 type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.FP64 type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.Str type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.Binary type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.Date type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.Time type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.TimestampTZ type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.Timestamp type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.PrecisionTimestamp type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.PrecisionTimestampTZ type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.IntervalYear type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.IntervalDay type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.UUID type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.FixedChar type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.VarChar type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.FixedBinary type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.Decimal type) throws RuntimeException {
+      return 0;
+    }
+
+    @Override
+    public Integer visit(Type.Struct type) throws RuntimeException {
+      // Only struct fields have names - the top level column names are also
+      // captured by this since the whole schema is wrapped in a Struct type
+      return type.fields().stream().mapToInt(field -> 1 + field.accept(this)).sum();
+    }
+
+    @Override
+    public Integer visit(Type.ListType type) throws RuntimeException {
+      return type.elementType().accept(this);
+    }
+
+    @Override
+    public Integer visit(Type.Map type) throws RuntimeException {
+      return type.key().accept(this) + type.value().accept(this);
+    }
+
+    @Override
+    public Integer visit(Type.UserDefined type) throws RuntimeException {
+      return 0;
+    }
   }
 }
