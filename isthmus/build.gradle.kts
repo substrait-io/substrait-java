2f36151ff411eaf3023382b716b74b91658c4c03
Victor Barua <victor.barua@gmail.com>
2025-02-13 12:45:50 -0800

    build: bump json-smart

--- isthmus/build.gradle.kts
+++ isthmus/build.gradle.kts
@@ -81,6 +81,10 @@ val PROTOBUF_VERSION = properties.get("protobuf.version")
 dependencies {
   implementation(project(":core"))
   implementation("org.apache.calcite:calcite-core:${CALCITE_VERSION}")
+  // calcite-core 1.37.0 brings in net.minidev:json-smart:2.5.0 which has a CVE associated with it.
+  // See: https://osv.dev/vulnerability/GHSA-pq2g-wx69-c263
+  // This causes the build to fail. Pull in the fixed version until Calcite is updated
+  implementation("net.minidev:json-smart:2.5.2")
   implementation("org.apache.calcite:calcite-server:${CALCITE_VERSION}")
   testImplementation("org.junit.jupiter:junit-jupiter:${JUNIT_VERSION}")
   implementation("org.reflections:reflections:0.9.12")
