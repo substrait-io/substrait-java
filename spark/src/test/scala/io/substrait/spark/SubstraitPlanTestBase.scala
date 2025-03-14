75f2c8f8f221efb4782339a63a344eea9393f630
Arttu Voutilainen <avoutilainen@palantir.com>
2025-03-14 16:04:04 +0000

    fix: use shouldEqualPlainly

--- spark/src/test/scala/io/substrait/spark/SubstraitPlanTestBase.scala
+++ spark/src/test/scala/io/substrait/spark/SubstraitPlanTestBase.scala
@@ -95,11 +95,11 @@ trait SubstraitPlanTestBase { self: SharedSparkSession =>
     val extensionCollector = new ExtensionCollector
     val bytes = new RelProtoConverter(extensionCollector).toProto(substraitPlan).toByteArray
 
-    // Read it back
+    // Read it back and ensure the proto roundtrip doesn't modify the plan
     val protoPlan = io.substrait.proto.Rel.parseFrom(bytes)
     val substraitPlan2 =
       new ProtoRelConverter(extensionCollector, SparkExtension.COLLECTION).from(protoPlan)
-    assertResult(substraitPlan)(substraitPlan2)
+    substraitPlan2.shouldEqualPlainly(substraitPlan)
 
     // convert substrait back to spark plan
     val sparkPlan2 = substraitPlan2.accept(new ToLogicalPlan(spark))
