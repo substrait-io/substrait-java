944be5e991e8c9cb319142602909e76f07caa675
Geoffrey Claude <geoffrey.claude@datadoghq.com>
2025-03-21 16:08:56 +0100

    feat: add `unquotedcasing` option to Isthmus CLI

--- isthmus/src/main/java/io/substrait/isthmus/SqlConverterBase.java
+++ isthmus/src/main/java/io/substrait/isthmus/SqlConverterBase.java
@@ -69,6 +69,7 @@ class SqlConverterBase {
     featureBoard = features == null ? FEATURES_DEFAULT : features;
     parserConfig =
         SqlParser.Config.DEFAULT
+            .withUnquotedCasing(featureBoard.unquotedCasing())
             .withParserFactory(SqlDdlParserImpl.FACTORY)
             .withConformance(featureBoard.sqlConformanceMode());
   }
