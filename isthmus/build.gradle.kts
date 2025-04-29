f8232a4d904b82537ead42150fe8af9dbd0fa372
Mark S. Lewis <Mark.S.Lewis@outlook.com>
2025-04-29 14:05:21 +0100

    fix(isthmus): upgrade to Calcite 1.39.0
    This version of Calcite improves support for nested window aggregation,
    which allows Isthmus to support several TPC-DS queries that were
    previously failing.
    
    Signed-off-by: Mark S. Lewis <Mark.S.Lewis@outlook.com>

--- isthmus/build.gradle.kts
+++ isthmus/build.gradle.kts
@@ -84,10 +84,13 @@ val PROTOBUF_VERSION = properties.get("protobuf.version")
 dependencies {
   api(project(":core"))
   api("org.apache.calcite:calcite-core:${CALCITE_VERSION}")
-  // calcite-core 1.37.0 brings in net.minidev:json-smart:2.5.0 which has a CVE associated with it.
-  // See: https://osv.dev/vulnerability/GHSA-pq2g-wx69-c263
-  // This causes the build to fail. Pull in the fixed version until Calcite is updated
-  implementation("net.minidev:json-smart:2.5.2")
+  constraints {
+    // calcite-core:1.39.0 has dependencies that contain vulnerabilities:
+    // - CVE-2025-27820 (org.apache.httpcomponents.client5:httpclient5 < 5.4.3)
+    // - CVE-2024-57699 (net.minidev:json-smart < 2.5.2)
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.4.4")
+    implementation("net.minidev:json-smart:2.5.2")
+  }
   implementation("org.apache.calcite:calcite-server:${CALCITE_VERSION}")
   testImplementation("org.junit.jupiter:junit-jupiter:${JUNIT_VERSION}")
   implementation("org.reflections:reflections:0.9.12")
