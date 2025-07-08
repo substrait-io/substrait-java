4be421bc5c6bfa935e13e5cb56c3eb4b6237882f
Anton Zorin <zor@zurich.ibm.com>
2025-07-08 10:25:12 +0200

    feat(istmus): add ddl support to sql->substrait
    # Conflicts:
    #	isthmus/src/main/java/io/substrait/isthmus/SqlToSubstrait.java
    
    # Conflicts:
    #	isthmus/src/main/java/io/substrait/isthmus/SqlToSubstrait.java
    
    # Conflicts:
    #	isthmus/src/main/java/io/substrait/isthmus/SqlToSubstrait.java
    #	isthmus/src/test/java/io/substrait/isthmus/DdlRoundtripTest.java

--- isthmus/src/test/java/io/substrait/isthmus/DdlRoundtripTest.java
+++ isthmus/src/test/java/io/substrait/isthmus/DdlRoundtripTest.java
@@ -2,12 +2,10 @@ package io.substrait.isthmus;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import io.substrait.isthmus.operation.CalciteOperation;
 import io.substrait.isthmus.sql.SubstraitCreateStatementParser;
 import io.substrait.plan.Plan;
 import io.substrait.plan.PlanProtoConverter;
 import io.substrait.plan.ProtoPlanConverter;
-import io.substrait.relation.Rel;
 import org.apache.calcite.prepare.Prepare;
 import org.apache.calcite.sql.parser.SqlParseException;
 import org.junit.jupiter.api.Test;
@@ -30,47 +28,15 @@ class DdlRoundtripTest extends PlanTestBase {
     assertEquals(protoPlan, protoPlan1);
   }
 
-  protected void assertCalciteOperationSubstraitRelRoundTrip(
-      String query, Prepare.CatalogReader catalogReader) throws Exception {
-    // sql <--> substrait round trip test.
-    // Assert (sql -> calcite -> substrait) and (sql -> substrait -> calcite -> substrait) are same.
-    // Return list of sql -> Substrait rel -> Calcite rel.
-    SqlToSubstrait s = new SqlToSubstrait();
-
-    // 1. SQL -> Substrait Plan
-    Plan plan1 = s.convert(query, catalogReader);
-
-    // 2. Substrait Plan  -> Substrait Rel
-    Plan.Root pojo1 = plan1.getRoots().get(0);
-
-    // 3. Substrait Rel -> CalciteOperation
-
-    SubstraitToCalciteOperation substraitToCalciteOperation =
-        new SubstraitToCalciteOperation(extensions, typeFactory, catalogReader, s.parserConfig);
-    Rel rel = plan1.getRoots().get(0).getInput();
-    CalciteOperation calciteOperation =
-        rel.accept(substraitToCalciteOperation, SubstraitRelNodeConverter.Context.newContext());
-
-    // 4. CalciteOperation -> Substrait Rel
-    CalciteOperationToSubstrait calciteOperationToSubstrait =
-        new CalciteOperationToSubstrait(extensions, s.featureBoard);
-    Plan.Root pojo2 = calciteOperation.accept(calciteOperationToSubstrait);
-
-    assertEquals(pojo1, pojo2);
-  }
-
-  void testPlanRoundTrip(String sqlStatement) throws Exception {
+  void testPlanRoundTrip(String sqlStatement) throws SqlParseException {
     SqlToSubstrait sql2subst = new SqlToSubstrait();
     final Plan plan = sql2subst.convert(sqlStatement, catalogReader);
 
     assertPlanRoundtrip(plan);
-    assertCalciteOperationSubstraitRelRoundTrip(sqlStatement, catalogReader);
-
-    //    assertFullRoundTrip(sqlStatement, catalogReader);
   }
 
   @Test
-  void testCreateTable() throws Exception {
+  void testCreateTable() throws SqlParseException {
     String sql = "create table dst1 as select * from src1";
     testSqlToSubstrait(sql);
     // TBD: full roundtrip is not possible because there is no relational algebra for DDL
@@ -78,7 +44,7 @@ class DdlRoundtripTest extends PlanTestBase {
   }
 
   @Test
-  void testCreateView() throws Exception {
+  void testCreateView() throws SqlParseException {
     String sql = "create view dst1 as select * from src1";
     testSqlToSubstrait(sql);
     testPlanRoundTrip(sql);
