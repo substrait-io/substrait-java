7906dae271faccccd11cd0d957c8ae9e7c4d0488
Niels Pardon <par@zurich.ibm.com>
2025-07-25 12:04:20 +0200

    fix(isthmus-cli): fix tpch_smoke.sh test script (#450)
    Signed-off-by: Niels Pardon <par@zurich.ibm.com>

--- isthmus-cli/src/test/script/tpch_smoke.sh
+++ isthmus-cli/src/test/script/tpch_smoke.sh
@@ -11,11 +11,8 @@ TPCH="../../../../isthmus/src/test/resources/tpch"
 DDL=$(cat ${TPCH}/schema.sql)
 QUERY_FOLDER="${TPCH}/queries"
 
-##for QUERYNUM in {1..22}; do
-# TODO: failed query: 8 12 15. 15 failed due to comments
-QUERY_TO_RUN=(1 2 3 4 5 6 7 9 10 11 13 14 16 17 18 19 20 21 22)
-for QUERY_NUM in "${QUERY_TO_RUN[@]}"; do
-     if [ "${QUERY_NUM}" -lt 10 ]; then
+for QUERY_NUM in {1..22}; do
+    if [ "${QUERY_NUM}" -lt 10 ]; then
        QUERY=$(cat "${QUERY_FOLDER}/0${QUERY_NUM}.sql")
      else
        QUERY=$(cat "${QUERY_FOLDER}/${QUERY_NUM}.sql")
@@ -23,5 +20,5 @@ for QUERY_NUM in "${QUERY_TO_RUN[@]}"; do
 
     echo "Processing tpc-h query ${QUERY_NUM}"
     echo "${QUERY}"
-    $CMD "${QUERY}" --create "${DDL}"
+    $CMD --create "${DDL}" -- "${QUERY}"
 done
