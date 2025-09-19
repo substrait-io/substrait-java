863acbe1132ce2d21dce2d8561b61e04bd40b8b1
Mark S. Lewis <Mark.S.Lewis@outlook.com>
2025-09-19 10:40:09 +0100

    build(isthmus-cli): use GraalVM 25
    Benefits from Java 25 performance optimizations.
    
    Signed-off-by: Mark S. Lewis <Mark.S.Lewis@outlook.com>

--- isthmus-cli/build.gradle.kts
+++ isthmus-cli/build.gradle.kts
@@ -44,89 +44,6 @@ dependencies {
 
 sourceSets { main { java.srcDir("build/generated/sources/version/") } }
 
-val initializeAtBuildTime =
-  listOf(
-      "com.google.common.base.Platform",
-      "com.google.common.base.Preconditions",
-      "com.google.common.cache.CacheBuilder",
-      "com.google.common.cache.LocalCache",
-      "com.google.common.collect.CollectCollectors",
-      "com.google.common.collect.ImmutableRangeSet",
-      "com.google.common.collect.ImmutableSortedMap",
-      "com.google.common.collect.Platform",
-      "com.google.common.collect.Range",
-      "com.google.common.collect.RegularImmutableMap",
-      "com.google.common.collect.RegularImmutableSortedSet",
-      "com.google.common.math.IntMath",
-      "com.google.common.math.IntMath\$1",
-      "com.google.common.primitives.Primitives",
-      "com.google.common.util.concurrent.AbstractFuture",
-      "com.google.common.util.concurrent.AbstractFuture\$UnsafeAtomicHelper",
-      "com.google.common.util.concurrent.SettableFuture",
-      "io.substrait.isthmus.cli.InitializeAtBuildTime",
-      "io.substrait.isthmus.metadata.LambdaHandlerCache",
-      "io.substrait.isthmus.metadata.LambdaMetadataSupplier",
-      "io.substrait.isthmus.metadata.LegacyToLambdaGenerator",
-      "org.apache.calcite.config.CalciteSystemProperty",
-      "org.apache.calcite.linq4j.tree.Primitive",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$AllPredicates",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$Collation",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$ColumnOrigin",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$ColumnUniqueness",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$CumulativeCost",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$DistinctRowCount",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$Distribution",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$ExplainVisibility",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$ExpressionLineage",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$LowerBoundCost",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$MaxRowCount",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$Memory",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$MinRowCount",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$NodeTypes",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$NonCumulativeCost",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$Parallelism",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$PercentageOriginalRows",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$PopulationSize",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$Predicates",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$RowCount",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$Selectivity",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$Size",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$TableReferences",
-      "org.apache.calcite.rel.metadata.BuiltInMetadata\$UniqueKeys",
-      "org.apache.calcite.rel.metadata.RelMdAllPredicates",
-      "org.apache.calcite.rel.metadata.RelMdCollation",
-      "org.apache.calcite.rel.metadata.RelMdColumnOrigins",
-      "org.apache.calcite.rel.metadata.RelMdColumnUniqueness",
-      "org.apache.calcite.rel.metadata.RelMdDistinctRowCount",
-      "org.apache.calcite.rel.metadata.RelMdDistribution",
-      "org.apache.calcite.rel.metadata.RelMdExplainVisibility",
-      "org.apache.calcite.rel.metadata.RelMdExpressionLineage",
-      "org.apache.calcite.rel.metadata.RelMdLowerBoundCost",
-      "org.apache.calcite.rel.metadata.RelMdMaxRowCount",
-      "org.apache.calcite.rel.metadata.RelMdMemory",
-      "org.apache.calcite.rel.metadata.RelMdMinRowCount",
-      "org.apache.calcite.rel.metadata.RelMdNodeTypes",
-      "org.apache.calcite.rel.metadata.RelMdParallelism",
-      "org.apache.calcite.rel.metadata.RelMdPercentageOriginalRows",
-      "org.apache.calcite.rel.metadata.RelMdPopulationSize",
-      "org.apache.calcite.rel.metadata.RelMdPredicates",
-      "org.apache.calcite.rel.metadata.RelMdRowCount",
-      "org.apache.calcite.rel.metadata.RelMdSelectivity",
-      "org.apache.calcite.rel.metadata.RelMdSize",
-      "org.apache.calcite.rel.metadata.RelMdTableReferences",
-      "org.apache.calcite.rel.metadata.RelMdUniqueKeys",
-      "org.apache.calcite.sql.SqlKind",
-      "org.apache.calcite.util.BuiltInMethod",
-      "org.apache.calcite.util.Pair",
-      "org.apache.calcite.util.ReflectUtil",
-      "org.apache.calcite.util.Util",
-      "org.apache.commons.codec.language.Soundex",
-      "org.slf4j.LoggerFactory",
-      "org.slf4j.impl.JDK14LoggerAdapter",
-      "org.slf4j.impl.StaticLoggerBinder",
-    )
-    .joinToString(",")
-
 application { mainClass.set("io.substrait.isthmus.cli.IsthmusEntryPoint") }
 
 graalvmNative {
@@ -138,13 +55,13 @@ graalvmNative {
     named("main") {
       imageName.set("isthmus")
       fallback.set(false)
-      buildArgs.add("--initialize-at-build-time=${initializeAtBuildTime}")
       buildArgs.add("-H:IncludeResources=.*yaml")
       buildArgs.add("--report-unsupported-elements-at-runtime")
       buildArgs.add("-H:+ReportExceptionStackTraces")
       buildArgs.add("-H:DynamicProxyConfigurationFiles=${project.file("proxies.json")}")
       buildArgs.add("--features=io.substrait.isthmus.cli.RegisterAtRuntime")
-      buildArgs.add("-J--enable-preview")
+      buildArgs.add("--future-defaults=all")
+      jvmArgs.add("--sun-misc-unsafe-memory-access=allow")
     }
   }
 }
--- isthmus-cli/src/test/script/smoke.sh
+++ isthmus-cli/src/test/script/smoke.sh
@@ -3,32 +3,32 @@
 set -eu -o pipefail
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "${parent_path}"
-CMD=../../../build/native/nativeCompile/isthmus
-LINEITEM="CREATE TABLE LINEITEM (L_ORDERKEY BIGINT NOT NULL, L_PARTKEY BIGINT NOT NULL, L_SUPPKEY BIGINT NOT NULL, L_LINENUMBER INTEGER, L_QUANTITY DECIMAL, L_EXTENDEDPRICE DECIMAL, L_DISCOUNT DECIMAL, L_TAX DECIMAL, L_RETURNFLAG CHAR(1), L_LINESTATUS CHAR(1), L_SHIPDATE DATE, L_COMMITDATE DATE, L_RECEIPTDATE DATE, L_SHIPINSTRUCT CHAR(25), L_SHIPMODE CHAR(10), L_COMMENT VARCHAR(44))"
+cd "${parent_path}/../../.."
+CMD="${ISTHMUS:-build/native/nativeCompile/isthmus}"
+LINEITEM='CREATE TABLE LINEITEM (L_ORDERKEY BIGINT NOT NULL, L_PARTKEY BIGINT NOT NULL, L_SUPPKEY BIGINT NOT NULL, L_LINENUMBER INTEGER, L_QUANTITY DECIMAL, L_EXTENDEDPRICE DECIMAL, L_DISCOUNT DECIMAL, L_TAX DECIMAL, L_RETURNFLAG CHAR(1), L_LINESTATUS CHAR(1), L_SHIPDATE DATE, L_COMMITDATE DATE, L_RECEIPTDATE DATE, L_SHIPINSTRUCT CHAR(25), L_SHIPMODE CHAR(10), L_COMMENT VARCHAR(44))'
 echo "${LINEITEM}"
 #set -x
 
 # SQL Query - Simple
-$CMD 'select * from lineitem' --create "${LINEITEM}"
+"${CMD}" 'select * from lineitem' --create "${LINEITEM}"
 
 # SQL Query - With condition
-$CMD 'select * from lineitem where l_orderkey > 10' --create "${LINEITEM}"
+"${CMD}" 'select * from lineitem where l_orderkey > 10' --create "${LINEITEM}"
 
 # SQL Query - Aggregate
-$CMD 'select l_orderkey, count(l_partkey) from lineitem group by l_orderkey' --create "${LINEITEM}"
+"${CMD}" 'select l_orderkey, count(l_partkey) from lineitem group by l_orderkey' --create "${LINEITEM}"
 
 # SQL Expression - Literal expression
-$CMD --expression '10'
+"${CMD}" --expression '10'
 
 # SQL Expression - Reference expression
-$CMD --expression 'l_suppkey' --create "${LINEITEM}"
+"${CMD}" --expression 'l_suppkey' --create "${LINEITEM}"
 
 # SQL Expression - Filter expression
-$CMD --expression 'l_orderkey > 10' --create "${LINEITEM}"
+"${CMD}" --expression 'l_orderkey > 10' --create "${LINEITEM}"
 
 # SQL Expression - Projection expression (column-1)
-$CMD --expression 'l_orderkey + 9888486986' --create "${LINEITEM}"
+"${CMD}" --expression 'l_orderkey + 9888486986' --create "${LINEITEM}"
 
 # SQL Expression - 03 Projection expression (column-1, column-2, column-3)
-$CMD --expression 'l_orderkey + 9888486986' 'l_orderkey * 2' 'l_orderkey > 10' 'l_orderkey in (10, 20)' --create "${LINEITEM}"
+"${CMD}" --expression 'l_orderkey + 9888486986' 'l_orderkey * 2' 'l_orderkey > 10' 'l_orderkey in (10, 20)' --create "${LINEITEM}"
