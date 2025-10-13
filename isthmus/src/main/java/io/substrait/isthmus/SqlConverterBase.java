6d9cc2a45a397190d061aceb2d1a150fa67d9205
Anton Zorin <zor@zurich.ibm.com>
2025-10-13 19:44:05 +0200

    chore: resolve pmd

--- isthmus/src/main/java/io/substrait/isthmus/SqlConverterBase.java
+++ isthmus/src/main/java/io/substrait/isthmus/SqlConverterBase.java
@@ -1,6 +1,5 @@
 package io.substrait.isthmus;
 
-import io.substrait.extension.DefaultExtensionCatalog;
 import io.substrait.extension.SimpleExtension;
 import org.apache.calcite.config.CalciteConnectionConfig;
 import org.apache.calcite.config.CalciteConnectionProperty;
