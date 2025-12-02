3504e48e182e90589b7f4f49d1e5e14f39cb721b
Gordon Hamilton <gordon.hamilton@datadoghq.com>
2025-12-02 09:40:55 -0500

    feat: fixing the isthmus conversion to and from the virtualTable

--- isthmus/src/test/java/io/substrait/isthmus/VirtualTableScanTest.java
+++ isthmus/src/test/java/io/substrait/isthmus/VirtualTableScanTest.java
@@ -4,22 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import io.substrait.dsl.SubstraitBuilder;
 import io.substrait.expression.Expression;
-import io.substrait.relation.Rel;
 import io.substrait.relation.VirtualTableScan;
 import io.substrait.type.NamedStruct;
-import io.substrait.type.Type;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.RelWriter;
 import org.apache.calcite.rel.externalize.RelWriterImpl;
 import org.apache.calcite.sql.SqlExplainLevel;
 import org.junit.jupiter.api.Test;
-import org.locationtech.jts.util.Assert;
 
 public class VirtualTableScanTest extends PlanTestBase {
 
@@ -47,54 +43,28 @@ public class VirtualTableScanTest extends PlanTestBase {
   }
 
   @Test
-  void expressionContainingVirtualTableNullable() {
-      //      set a field to nullable
-    NamedStruct schema = NamedStruct.of(List.of("col1", "col2"), R.struct(N.I32, R.FP64));
-      var a = Expression.I32Literal.builder().nullable(true).value(2).build();
-      var x = b.multiply(b.i32(6), b.i32(2));
-      var c = Expression.ScalarFunctionInvocation.builder().from(x).outputType(Type.I32.builder().nullable(true).build()).build();
-
-      //    calcite is not allowing nullable types in fields
+  void expressionContainingVirtualTable() {
+    NamedStruct schema = NamedStruct.of(List.of("col1", "col2"), R.struct(R.I32, R.FP64));
     VirtualTableScan virtualTableScan =
         createVirtualTableScan(
             schema,
-                List.of(a, b.add(b.fp64(4.4), b.fp64(4.5))),
-                List.of(c, b.fp64(8.8)));
-
+            List.of(b.i32(2), b.add(b.fp64(4.4), b.fp64(4.5))),
+            List.of(b.multiply(b.i32(6), b.i32(2)), b.fp64(8.8)));
 
     // Check the specific Calcite encoding
     RelNode relNode = substraitToCalcite.convert(virtualTableScan);
     assertEquals(
-        "LogicalExpressions(type=[RecordType(INTEGER col1, DOUBLE col2)], tuples=[[{ 2, +(4.4E0:DOUBLE, 4.5E0:DOUBLE) }, { *(6, 2), 8.8E0:DOUBLE }]])\n",
+        "LogicalProject(inputs=[0..1])\n"
+            + "  LogicalUnion(all=[true])\n"
+            + "    LogicalProject(exprs=[[2, +(4.4E0:DOUBLE, 4.5E0:DOUBLE)]])\n"
+            + "      LogicalValues(type=[RecordType()], tuples=[[{  }]])\n"
+            + "    LogicalProject(exprs=[[*(6, 2), 8.8E0:DOUBLE]])\n"
+            + "      LogicalValues(type=[RecordType()], tuples=[[{  }]])\n",
         explain(relNode));
 
     // Check full roundtrip conversion
     assertFullRoundTrip(virtualTableScan);
   }
-
-    @Test
-    void expressionContainingVirtualTable() {
-        NamedStruct schema = NamedStruct.of(List.of("col1", "col2"), R.struct(R.I32, R.FP64));
-        VirtualTableScan virtualTableScan =
-                createVirtualTableScan(
-                        schema,
-                        List.of(b.i32(2), b.add(b.fp64(4.4), b.fp64(4.5))),
-                        List.of(b.multiply(b.i32(6), b.i32(2)), b.fp64(8.8)));
-
-        // Check the specific Calcite encoding
-        RelNode relNode = substraitToCalcite.convert(virtualTableScan);
-        assertEquals(
-                "LogicalProject(inputs=[0..1])\n" +
-                        "  LogicalUnion(all=[true])\n" +
-                        "    LogicalProject(exprs=[[2, +(4.4E0:DOUBLE, 4.5E0:DOUBLE)]])\n" +
-                        "      LogicalValues(type=[RecordType()], tuples=[[{  }]])\n" +
-                        "    LogicalProject(exprs=[[*(6, 2), 8.8E0:DOUBLE]])\n" +
-                        "      LogicalValues(type=[RecordType()], tuples=[[{  }]])\n",
-                explain(relNode));
-
-        // Check full roundtrip conversion
-        assertFullRoundTrip(virtualTableScan);
-    }
 
   @SafeVarargs
   private VirtualTableScan createVirtualTableScan(NamedStruct schema, List<Expression>... rows) {
