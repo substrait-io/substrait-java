7c3abe830d914c96b03bb054d26ca4a5f0314bcc
Victor Barua <victor.barua@datadoghq.com>
2025-12-04 16:49:58 -0800

    test: prefer assertFullRoundTrip over assertSqlSubstraitRelRoundTrip (#626)

--- isthmus/src/test/java/io/substrait/isthmus/LogarithmicFunctionTest.java
+++ isthmus/src/test/java/io/substrait/isthmus/LogarithmicFunctionTest.java
@@ -12,20 +12,20 @@ class LogarithmicFunctionTest extends PlanTestBase {
   @ValueSource(strings = {"fp32", "fp64"})
   void ln(String column) throws Exception {
     String query = String.format("SELECT ln(%s) FROM numbers", column);
-    assertSqlSubstraitRelRoundTrip(query, CREATES);
+    assertFullRoundTrip(query, CREATES);
   }
 
   @ParameterizedTest
   @ValueSource(strings = {"fp32", "fp64"})
   void log10(String column) throws Exception {
     String query = String.format("SELECT log10(%s) FROM numbers", column);
-    assertSqlSubstraitRelRoundTrip(query, CREATES);
+    assertFullRoundTrip(query, CREATES);
   }
 
   @ParameterizedTest
   @ValueSource(strings = {"i64", "fp32", "fp64"})
   void log2(String column) throws Exception {
     String query = String.format("SELECT log2(%s) FROM numbers", column);
-    assertSqlSubstraitRelRoundTrip(query, CREATES);
+    assertFullRoundTrip(query, CREATES);
   }
 }
