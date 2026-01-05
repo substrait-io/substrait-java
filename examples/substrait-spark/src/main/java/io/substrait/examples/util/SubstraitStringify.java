8a06e8bca5b2c0a42fc1c7a870b0e3ef481164fe
MBWhite <whitemat@uk.ibm.com>
2026-01-05 16:31:10 +0000

    fix: review comments
    Signed-off-by: MBWhite <whitemat@uk.ibm.com>

--- examples/substrait-spark/src/main/java/io/substrait/examples/util/SubstraitStringify.java
+++ examples/substrait-spark/src/main/java/io/substrait/examples/util/SubstraitStringify.java
@@ -71,8 +71,10 @@ public class SubstraitStringify extends ParentStringify
   }
 
   /**
-   * Explains the Sustrait plan
+   * Explains the Substrait plan
    *
+   * @param plan Substrait Plan
+   * 
    * @return List of strings; typically these would then be logged or sent to stdout
    */
   public static List<String> explain(io.substrait.plan.Plan plan) {
@@ -92,9 +94,9 @@ public class SubstraitStringify extends ParentStringify
   }
 
   /**
-   * Explains the Sustrait relation
+   * Explains the Substrait relation
    *
-   * @param rel Subsrait relation
+   * @param rel Substrait relation
    * @return List of strings; typically these would then be logged or sent to stdout
    */
   public static List<String> explain(io.substrait.relation.Rel rel) {
