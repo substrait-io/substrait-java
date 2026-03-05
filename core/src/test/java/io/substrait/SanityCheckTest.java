83b9d9d4cb257d2871ee58b9c8c8b270505b2ce9
Ben Bellick <ben.bellick@datadoghq.com>
2026-03-05 13:47:40 -0500

    proof: add deliberately failing core test to demonstrate #725

---
+++ core/src/test/java/io/substrait/SanityCheckTest.java
@@ -0,0 +1,12 @@
+package io.substrait;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class SanityCheckTest {
+  @Test
+  public void thisTestShouldFail() {
+    fail("This test should fail - sanity check");
+  }
+}
