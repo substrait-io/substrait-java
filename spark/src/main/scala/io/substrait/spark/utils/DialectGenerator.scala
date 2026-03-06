9f9877c9e18409a99f4972fae48c53f33123a6e2
Victor Barua <victor.barua@datadoghq.com>
2026-03-05 18:14:42 -0800

    feat: update spark_dialact with max_precision

--- spark/src/main/scala/io/substrait/spark/utils/DialectGenerator.scala
+++ spark/src/main/scala/io/substrait/spark/utils/DialectGenerator.scala
@@ -7,8 +7,8 @@ import io.substrait.spark.expression.Sig
 import org.apache.spark.sql.catalyst.expressions.{BinaryOperator, Expression, Literal}
 import org.apache.spark.sql.types.{ByteType, DateType, DayTimeIntervalType, DoubleType, FloatType, IntegerType, LongType, NullType, ShortType, TimestampNTZType, TimestampType, YearMonthIntervalType}
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.networknt.schema.{InputFormat, SchemaRegistry, SpecificationVersion}
 import io.substrait.extension.SimpleExtension
@@ -29,7 +29,7 @@ case class Dialect(
 
 // Types section
 case class TypeMetadata(name: String, supported_as_column: Boolean)
-case class SupportedType(`type`: String, system_metadata: TypeMetadata)
+case class SupportedType(`type`: String, system_metadata: TypeMetadata, max_precision: Option[Integer] = None)
 
 // Functions section
 case class FunctionMetadata(name: String, notation: String)
@@ -83,7 +83,12 @@ class DialectGenerator {
 
   def generateYaml(): String = {
     // Generate the dialect YAML
-    val mapper = new ObjectMapper(new YAMLFactory()).registerModules(DefaultScalaModule)
+    val mapper = YAMLMapper
+      .builder()
+      .defaultPropertyInclusion(
+        JsonInclude.Value.ALL_NON_ABSENT)
+      .addModule(DefaultScalaModule)
+      .build()
     val yaml = mapper.writeValueAsString(generate())
 
     // Validate against the substrait dialect schema
@@ -113,9 +118,9 @@ class DialectGenerator {
       SupportedType("FIXED_CHAR", TypeMetadata("StringType", true)),
       SupportedType("BINARY", TypeMetadata("BinaryType", true)),
       SupportedType("BOOL", TypeMetadata("BooleanType", true)),
-      SupportedType("PRECISION_TIMESTAMP", TypeMetadata("TimestampNTZType", true)),
-      SupportedType("PRECISION_TIMESTAMP_TZ", TypeMetadata("TimestampType", true)),
-      SupportedType("INTERVAL_DAY", TypeMetadata("DayTimeIntervalType", true)),
+      SupportedType("PRECISION_TIMESTAMP", TypeMetadata("TimestampNTZType", true), max_precision = Some(9)),
+      SupportedType("PRECISION_TIMESTAMP_TZ", TypeMetadata("TimestampType", true), max_precision = Some(9)),
+      SupportedType("INTERVAL_DAY", TypeMetadata("DayTimeIntervalType", true), max_precision = Some(9)),
       SupportedType("INTERVAL_YEAR", TypeMetadata("YearMonthIntervalType", true)),
       SupportedType("LIST", TypeMetadata("ArrayType", true)),
       SupportedType("MAP", TypeMetadata("MapType", true)),
