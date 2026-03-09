19f3b0d79a55ed6aafcabc1264aa2e51d52f7c56
Niels Pardon <par@zurich.ibm.com>
2026-03-09 09:14:54 +0100

    fix: apply suggestion from @nielspardon

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -226,7 +226,9 @@ tasks.named<Jar>("sourcesJar") {
 
 sourceSets {
   main {
-    antlr.srcDirs("../substrait/grammar")
+    antlr {
+      setSrcDirs(listOf(file("${rootProject.projectDir}/substrait/grammar")))
+    }
     proto.srcDir("../substrait/proto")
     resources.srcDir("../substrait/extensions")
     resources.srcDir("build/generated/sources/manifest/")
