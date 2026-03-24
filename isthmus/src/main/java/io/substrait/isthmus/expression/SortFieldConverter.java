434fe29ec9578dff214d3b1c8551d44731d53e27
Matthew B White <whitemat@uk.ibm.com>
2026-03-24 09:53:57 +0000

    docs(isthmus): javadoc for io.substrait.isthmus.expression classes (#764)
    Signed-off-by: MBWhite <whitemat@uk.ibm.com>

--- isthmus/src/main/java/io/substrait/isthmus/expression/SortFieldConverter.java
+++ isthmus/src/main/java/io/substrait/isthmus/expression/SortFieldConverter.java
@@ -5,9 +5,22 @@ import org.apache.calcite.rel.RelFieldCollation;
 import org.apache.calcite.rel.RelFieldCollation.Direction;
 import org.apache.calcite.rex.RexFieldCollation;
 
+/**
+ * Utility for converting Calcite {@link RexFieldCollation} objects into Substrait {@link
+ * Expression.SortField} representations.
+ *
+ * <p>Handles sort direction and null ordering.
+ */
 public class SortFieldConverter {
 
-  /** Converts a {@link RexFieldCollation} to a {@link Expression.SortField}. */
+  /**
+   * Converts a Calcite {@link RexFieldCollation} to a Substrait {@link Expression.SortField}.
+   *
+   * @param rexFieldCollation The Calcite field collation to convert.
+   * @param rexExpressionConverter Converter for translating the field expression.
+   * @return A Substrait {@link Expression.SortField} with the appropriate direction and expression.
+   * @throws IllegalArgumentException if the collation direction is unsupported.
+   */
   public static Expression.SortField toSortField(
       RexFieldCollation rexFieldCollation, RexExpressionConverter rexExpressionConverter) {
     Expression expr = rexFieldCollation.left.accept(rexExpressionConverter);
@@ -16,6 +29,13 @@ public class SortFieldConverter {
     return Expression.SortField.builder().expr(expr).direction(direction).build();
   }
 
+  /**
+   * Determines the Substrait {@link Expression.SortDirection} based on Calcite collation details.
+   *
+   * @param collation The Calcite {@link RexFieldCollation}.
+   * @return The corresponding Substrait sort direction.
+   * @throws IllegalArgumentException if the direction is not ASCENDING or DESCENDING.
+   */
   private static Expression.SortDirection asSortDirection(RexFieldCollation collation) {
     RelFieldCollation.Direction direction = collation.getDirection();
 
