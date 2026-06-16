8f439776c42d0c1be6c2f89f369afd6cab024199
Niels Pardon <par@zurich.ibm.com>
2026-06-16 14:19:59 +0200

    docs(core): fix javadoc in ExtensionLeaf
    I used AI to generate missing javadoc comments for
    `core/src/main/java/io/substrait/relation/ExtensionLeaf.java`.
    
    Signed-off-by: Niels Pardon <par@zurich.ibm.com>

--- core/src/main/java/io/substrait/relation/ExtensionLeaf.java
+++ core/src/main/java/io/substrait/relation/ExtensionLeaf.java
@@ -3,9 +3,15 @@ package io.substrait.relation;
 import io.substrait.util.VisitationContext;
 import org.immutables.value.Value;
 
+/** A leaf relation whose behavior is supplied by an extension-defined detail object. */
 @Value.Immutable
 public abstract class ExtensionLeaf extends ZeroInputRel {
 
+  /**
+   * Returns the extension-specific detail describing this leaf relation.
+   *
+   * @return the extension detail object
+   */
   public abstract Extension.LeafRelDetail getDetail();
 
   @Override
@@ -14,6 +20,12 @@ public abstract class ExtensionLeaf extends ZeroInputRel {
     return visitor.visit(this, context);
   }
 
+  /**
+   * Creates a builder initialized from the given extension detail.
+   *
+   * @param detail the extension detail describing the leaf relation
+   * @return a builder populated from {@code detail}
+   */
   public static ImmutableExtensionLeaf.Builder from(Extension.LeafRelDetail detail) {
     return ImmutableExtensionLeaf.builder()
         .detail(detail)
