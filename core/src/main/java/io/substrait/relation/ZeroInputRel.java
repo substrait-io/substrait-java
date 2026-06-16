d151cc7468ae89d4e8ca8fbd7fb7bdb0e3f38aaf
Niels Pardon <par@zurich.ibm.com>
2026-06-16 14:00:57 +0200

    docs(core): fix javadoc in ZeroInputRel
    I used AI to generate missing javadoc comments for
    `core/src/main/java/io/substrait/relation/ZeroInputRel.java`.
    
    Signed-off-by: Niels Pardon <par@zurich.ibm.com>

--- core/src/main/java/io/substrait/relation/ZeroInputRel.java
+++ core/src/main/java/io/substrait/relation/ZeroInputRel.java
@@ -3,6 +3,10 @@ package io.substrait.relation;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Base class for relations that have no input relations, such as leaf relations that read directly
+ * from a source.
+ */
 public abstract class ZeroInputRel extends AbstractRel {
 
   @Override
