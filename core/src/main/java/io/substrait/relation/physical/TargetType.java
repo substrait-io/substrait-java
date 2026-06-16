f193658e9772b2cb483d5a1698f36b7f74db8f54
Niels Pardon <par@zurich.ibm.com>
2026-06-16 17:16:16 +0200

    docs(core): fix javadoc in TargetType (#917)

--- core/src/main/java/io/substrait/relation/physical/TargetType.java
+++ core/src/main/java/io/substrait/relation/physical/TargetType.java
@@ -2,22 +2,45 @@ package io.substrait.relation.physical;
 
 import org.immutables.value.Value;
 
+/** Identifies the destination of an exchange relation. */
 @Value.Enclosing
 public interface TargetType {
 
+  /** A target identified by a URI. */
   @Value.Immutable
   abstract class Uri implements TargetType {
+    /**
+     * Returns the URI identifying the target.
+     *
+     * @return the target URI
+     */
     public abstract String getUri();
 
+    /**
+     * Creates a builder for {@link Uri}.
+     *
+     * @return a new builder
+     */
     public static ImmutableTargetType.Uri.Builder builder() {
       return ImmutableTargetType.Uri.builder();
     }
   }
 
+  /** A target described by an extension-defined message. */
   @Value.Immutable
   abstract class Extended implements TargetType {
+    /**
+     * Returns the extension-defined message describing the target.
+     *
+     * @return the extension target message
+     */
     public abstract com.google.protobuf.Any getExtended();
 
+    /**
+     * Creates a builder for {@link Extended}.
+     *
+     * @return a new builder
+     */
     public static ImmutableTargetType.Extended.Builder builder() {
       return ImmutableTargetType.Extended.builder();
     }
