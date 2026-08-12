186ef55b463480fae15c46ab2f924b202aa15c82
Anas Ismail Khan <anasismail28@gmail.com>
2026-08-13 00:04:57 +0500

    fix(core): make ProtoRelConverter.newTopN keep relAnchor on the way back from proto (#1088)

--- core/src/main/java/io/substrait/relation/ProtoRelConverter.java
+++ core/src/main/java/io/substrait/relation/ProtoRelConverter.java
@@ -1085,7 +1085,8 @@ public class ProtoRelConverter {
     builder
         .commonExtension(optionalAdvancedExtension(rel.getCommon()))
         .remap(optionalRelmap(rel.getCommon()))
-        .hint(optionalHint(rel.getCommon()));
+        .hint(optionalHint(rel.getCommon()))
+        .relAnchor(optionalRelAnchor(rel.getCommon()));
     if (rel.hasAdvancedExtension()) {
       builder.extension(protoExtensionConverter.fromProto(rel.getAdvancedExtension()));
     }
