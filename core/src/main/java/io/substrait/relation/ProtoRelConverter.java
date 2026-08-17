08455723f9825d4117c689fbb394bde34599b9d6
Anas Ismail Khan <anasismail28@gmail.com>
2026-08-17 15:35:43 +0500

    register scope early in newNamedUpdate

--- core/src/main/java/io/substrait/relation/ProtoRelConverter.java
+++ core/src/main/java/io/substrait/relation/ProtoRelConverter.java
@@ -536,6 +536,8 @@ public class ProtoRelConverter {
     NamedStruct tableSchema = newNamedStruct(rel.getTableSchema());
     ProtoExpressionConverter converter =
         new ProtoExpressionConverter(lookup, extensions, tableSchema.struct(), this);
+    optionalRelAnchor(rel.getCommon())
+        .ifPresent(anchor -> anchorScopes.put(anchor, tableSchema.struct()));
     List<NamedUpdate.TransformExpression> transformations =
         new ArrayList<>(rel.getTransformationsCount());
     for (UpdateRel.TransformExpression transformation : rel.getTransformationsList()) {
